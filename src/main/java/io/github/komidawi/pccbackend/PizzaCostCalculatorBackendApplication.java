36a601ca53ab6ef2023ced8f19ca73b6abd9838f
Dawid Komisarczuk <komidawi@gmail.com>
2023-08-22 21:19:22 +0200

    Initial commit

---
+++ src/main/java/io/github/komidawi/pccbackend/PizzaCostCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.komidawi.pccbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaCostCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaCostCalculatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/komidawi/pccbackend/PizzaCostCalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.komidawi.pccbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaCostCalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
